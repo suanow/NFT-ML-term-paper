e3baf0d18457c45f3269f1805b2d1375e7a2a318
suanow <suanwow@gmail.com>
2023-06-05 22:44:27 +0300

    some pretty functions

--- models/ETH_parser.py
+++ models/ETH_parser.py
@@ -2,8 +2,10 @@
 
 import requests
 import json
+import datetime
+from datetime import datetime
 
-from conf.conf import logging
+from conf import logging
 
 
 API_key = "vYHU2nVXBMrIHlByeFOQWubnRQcl_v_3"
@@ -37,6 +39,8 @@ def get_sales_collection(API: str, address: str, limit: int) -> json:
     
     activity = [{"tokenId": sale["tokenId"],
                  "marketplace": sale["marketplace"],
+                 "block": sale["blockNumber"],
+                 "token": sale["sellerFee"]["symbol"],
                  "price": 0 if sale["sellerFee"]["amount"] is None else int(sale["sellerFee"]["amount"]) / 10 ** 18} for sale in data["nftSales"]]
 
     return activity
@@ -96,9 +100,24 @@ def get_rarity(API: str, address: str, tokenId: int) -> json:
     return result
 
 
-#print(get_activity_collection(API=API_key, address=address_bayc, limit=10))
-#print(get_collection_attributes(API=API_key, address=address_bayc))
-#print(get_sales_collection(API=API_key, address=address_bayc, limit=1))
-print(get_attributes(API=API_key, address=address_bayc, tokenId=8230))
-#print(get_attributes_collection(API=API_key, address=address_bayc))
-#print(get_rarity(API=API_key, address=address_bayc, tokenId=8230))
+def get_block_timestamp(block_number):
+    url = f"https://eth-mainnet.alchemyapi.io/v2/{API_key}"
+    payload = json.dumps({
+        "jsonrpc": "2.0",
+        "method": "eth_getBlockByNumber",
+        "params": [hex(block_number), False],
+        "id": 1
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    if response.status_code == 200:
+        timestamp = response.json()['result']['timestamp']
+        date_time = datetime.utcfromtimestamp(int(timestamp, 16))
+        return date_time
+    else:
+        return "error"
+
+
